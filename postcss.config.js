84f02d1b1ff1bd3eadc8804df7bc04eb4c6c6626
Bodhish Thomas <bodhish@gmail.com>
2021-06-06 11:55:37 +0530

    Cleanup custom purge

--- postcss.config.js
+++ postcss.config.js
@@ -1,29 +1,3 @@
-const cssnano = require("cssnano");
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: [
-    './src/**/*.re'
-  ],
-  // This is the function used to extract class names from your templates
-  defaultExtractor: content => {
-    // Capture as liberally as possible, including things like `h-(screen-1.5)`
-    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-
-    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
-
-    return broadMatches.concat(innerMatches)
-  }
-});
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    cssnano({
-      preset: "default",
-    }),
-    ...process.env.NODE_ENV === 'production'
-      ? [purgecss]
-      : []
-  ],
+  plugins: [require("tailwindcss"), require("autoprefixer")],
 };
-
